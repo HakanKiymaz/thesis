7fdeae8cb5ce1a29f6c1ea0ed797e067f2c66acd
hkiymaz <hakankiymaz20@gmail.com>
2023-05-21 02:26:17 +0300

    single lstm fix

--- lstm_single_task.py
+++ lstm_single_task.py
@@ -1,6 +1,4 @@
-import os
 import numpy as np
-from datetime import date
 import torch.nn as nn
 import torch
 import pandas as pd
@@ -15,6 +13,8 @@ from helpers import create_kidney_label, preprocess
 
 arguments = Args(args, type="kidney")
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 class SingleTaskLSTM(nn.Module):
     def __init__(self, arg_class, num_classes):
@@ -28,7 +28,7 @@ class SingleTaskLSTM(nn.Module):
         self.embed = nn.Embedding(emb_size, arg_class.emb_dim)
         self.lstm = nn.LSTM(input_size=arg_class.emb_dim, hidden_size=arg_class.hid_dim,
                             bidirectional=arg_class.bidirectional, batch_first=True)
-        self.fc1 = nn.Linear(arg_class.hid_dim*self.D, num_classes)
+        self.fc1 = nn.Linear(arg_class.hid_dim * self.D, num_classes)
         self.drop = nn.Dropout(0.3)
         self.activation = nn.ReLU()
         self.hidden_dim = arg_class.hid_dim
@@ -36,18 +36,40 @@ class SingleTaskLSTM(nn.Module):
         self.sigmoid = nn.Sigmoid()
         self.softmax = nn.Softmax()
 
-    def forward(self, x):
+    def forward(self, x, batch_mask):
         current_batch_size = len(x)
-        embed = self.embed(x)
+        embed = self.embed(x)  # Get embedding features of input ids
+
+        # mask filter for PADs.
+        embed = embed.permute((2, 0, 1))
+        embed = embed * batch_mask
+        embed = embed.permute((1, 2, 0))
+
         h0 = self.init_hidden(current_batch_size)
         c0 = self.init_cell(current_batch_size)
-        lstm_out, (hidden, cell) = self.lstm(embed, (h0, c0))
-        # hidden = torch.permute(hidden, (1, 0, 2))
+        lstm_out, (hidden, cell) = self.lstm(embed, (
+        h0, c0))  # lstm takes input features matrix and (hidden,cell) state vectors
+        # mask filtered embeddings are fed into lstm
+
+        # mask filter for PADs.
+        lstm_out = lstm_out.permute((2, 0, 1))  # permute for matrix multiplication.
+        lstm_out = lstm_out * batch_mask
+        lstm_out = lstm_out.permute((1, 2, 0))
+
         output = self.drop(lstm_out)
         output = self.activation(output)
-        output = self.fc1(output)
-        output = torch.squeeze(output)
-        return torch.sum(output, dim=1) # dim=0 or dim=1 ?????
+        output = self.fc1(output)  # fully connected projection layer for class logits
+
+        # mask filter for PADs.
+        output = output.permute((2, 0, 1))
+        output = output * batch_mask
+        output = output.permute((1, 2, 0))
+
+        output = torch.squeeze(output)  # squeeze into 1 single score. (pooling)
+        if current_batch_size == 1:
+            output = output.view((1, output.shape[0], output.shape[1]))
+        # TODO: pooling strategy can be wrong. should work on
+        return torch.sum(output, dim=1)
 
     def init_hidden(self, batch_size):
         return torch.zeros(self.D, batch_size, self.hidden_dim, requires_grad=False)
@@ -64,18 +86,32 @@ class PathologyDataset(Dataset):
         self.texts = tokenize(self.data.text.values.tolist(), max_length=max_len, padding="max_length",
                               truncation=True,
                               return_tensors="pt")
+        """
+        We use Bert tokenizer from huggingface.
+        Tokenizer gives 3 outputs which are pytorch tensors. 
+        (input_ids, token_type_ids, attention_mask)
+        """
         self.input_ids = self.texts["input_ids"]
         self.token_type_ids = self.texts["token_type_ids"]
         self.attention_mask = self.texts["attention_mask"]
-        self.labels = self.data.side.values.tolist()
+
+        self.labels_ = self.data.side.values.tolist()
         self.label_map = {"right": torch.Tensor([1, 0]),
                           "left": torch.Tensor([0, 1])}
-        print("label_map", self.label_map)
+        self.labels = [self.label_map[lbl] for lbl in self.labels_]
+        self.labels = torch.stack(self.labels)
+
+        # send to device to use GPU
+        self.input_ids.to(device)
+        self.attention_mask.to(device)
+        self.labels.to(device)
 
     def __getitem__(self, idx):
+        # this method will be called when we iterate dataset in the for loop.
         batch_text = [text for text in self.input_ids][idx]
-        batch_label = [self.label_map[lbl] for lbl in self.labels][idx]
-        return batch_text, batch_label
+        batch_label = [label for label in self.labels][idx]
+        batch_mask = [mask for mask in self.attention_mask][idx]
+        return batch_text, batch_label, batch_mask
 
     def __len__(self):
         return len(self.data)
@@ -86,9 +122,9 @@ def save_model(instance, path_to_save):
 
 
 if __name__ == "__main__":
-    result_path = "results/"
-    day_string = date.today().strftime("%Y_%m_%d")
-    checkpoint_path = result_path + day_string + "_checkpoints"
+    # result_path = "results/"
+    # day_string = date.today().strftime("%Y_%m_%d")
+    # checkpoint_path = result_path + day_string + "_checkpoints"
     # if os.path.isdir(checkpoint_path):
     #     raise Exception("Model Path Already Exist")
     #
@@ -98,7 +134,7 @@ if __name__ == "__main__":
                                  usecols=["Pateint_ID_text", "TEXT", "Anatomical_Position_type"])
 
     tabular_data["side"] = tabular_data["Anatomical_Position_type"].apply(create_kidney_label)
-    #filter if side == None
+
     tabular_data = tabular_data[tabular_data["side"] != "None"]
     tabular_data["text"] = tabular_data["TEXT"].apply(preprocess)
 
@@ -109,11 +145,14 @@ if __name__ == "__main__":
     # eval_data.to_excel(arguments.eval_data_path, index=False)
     model = SingleTaskLSTM(arguments,
                            num_classes=2)
+    model.to(device)
 
     train_data.drop("Pateint_ID_text", axis=1, inplace=True)
     eval_data.drop("Pateint_ID_text", axis=1, inplace=True)
+
     train_dataset = PathologyDataset(train_data, max_len=arguments.max_seq_len, tokenize=model.tokenizer)
     eval_dataset = PathologyDataset(eval_data, max_len=arguments.max_seq_len, tokenize=model.tokenizer)
+
     train_dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True)
     eval_dataloader = DataLoader(eval_dataset, batch_size=4, shuffle=True)
 
@@ -123,29 +162,38 @@ if __name__ == "__main__":
     for epoch in range(arguments.epochs):
         train_loss = []
         eval_loss = []
-        #os.mkdir(checkpoint_path + f"/{str(epoch)}")
+        # os.mkdir(checkpoint_path + f"/{str(epoch)}")
         model.train()
-        for features, labels in train_dataloader:
+        for features, labels, batch_mask in train_dataloader:
+            if features.shape[0] == 1:
+                labels = labels
+            else:
+                labels = labels.squeeze()
             optimizer.zero_grad()
 
-            out = model(features)
+            out = model(features, batch_mask)
             out = model.softmax(out)
-            loss = criterion(out, labels.squeeze())
+            loss = criterion(out, labels)
 
             loss.backward()
             optimizer.step()
             train_loss.append(loss.item())
-        losses["train_loss"].append(np.mean(train_loss))
+            losses["train_loss"].append(np.mean(train_loss))
         model.eval()
         with torch.no_grad():
-            for features, labels in eval_dataloader:
-                out = model(features)
-                loss = criterion(out, labels.squeeze())
+            for features, labels, batch_mask in eval_dataloader:
+                if features.shape[0] == 1:
+                    labels = labels
+                    print("wait")
+                else:
+                    labels = labels.squeeze()
+                out = model(features, batch_mask)
+                loss = criterion(out, labels)
                 eval_loss.append(loss.item())
-        losses["eval_loss"].append(np.mean(eval_loss))
-
-        save_model(model, checkpoint_path + f"/{str(epoch)}/" + "cp_kidney_cancer_type_lstm.pt")
-        with open(checkpoint_path + "/train_loss.txt", "w") as f:
-            f.write(str(np.mean(train_loss)) + "\n")
-        with open(checkpoint_path + "/eval_loss.txt", "w") as f:
-            f.write(str(np.mean(eval_loss)) + "\n")
+                losses["eval_loss"].append(np.mean(eval_loss))
+        print()
+        # save_model(model, checkpoint_path + f"/{str(epoch)}/" + "cp_kidney_cancer_type_lstm.pt")
+        # with open(checkpoint_path + "/train_loss.txt", "w") as f:
+        #     f.write(str(np.mean(train_loss)) + "\n")
+        # with open(checkpoint_path + "/eval_loss.txt", "w") as f:
+        #     f.write(str(np.mean(eval_loss)) + "\n")
